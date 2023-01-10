bebb8b979a28ccee11ad4d7d78d81b9a47a443f5
Eviive <albert.vaillon21@gmail.com>
2023-01-10 14:44:05 +0100

    Added plant method

--- ABR.py
+++ ABR.py
@@ -1,6 +1,33 @@
-from typing import Optional
 from binarytree import Node
 
 class ABR(Node):
-	def __init__(self, value: int, left: Optional[Node] = None, right: Optional[Node] = None) -> None:
-		super().__init__(value, left, right)
+	def __init__(self, values: list[int]):
+		super().__init__(values[0])
+
+		self.plants(values[1:])
+
+	def plant(self, value: int) -> bool:
+		node = self
+
+		while node is not None:
+			if value <= node.value:
+				if node.left is None:
+					node.left = Node(value)
+					return True
+				else:
+					node = node.left
+			else:
+				if node.right is None:
+					node.right = Node(value)
+					return True
+				else:
+					node = node.right
+
+		return False
+
+	def plants(self, values: list[int]) -> bool:
+		for value in values:
+			if not self.plant(value):
+				return False
+
+		return True
