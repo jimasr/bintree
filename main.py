e280a123e10d47daddc93bfc59897133207abf4a
Eviive <albert.vaillon21@gmail.com>
2023-01-10 15:08:00 +0100

    Updated main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from ABR import ABR
 
-if __name__ == '__main__':
+def main():
 	VALUES = [21, 8, 9, 11, 15, 19, 20, 7, 3, 2, 1, 5, 6, 4, 13, 14, 10, 12, 17, 16, 18]
 
 	tree = ABR(VALUES)
 
 	print(tree)
 
-	print(tree.search(4))
-	print(tree.search(100))
+	print(f"Searching for 4: {tree.search(4)}")
+	print(f"Searching for 100: {tree.search(100)}")
 
-	print(tree.sort())
+	print(f"Sorted values: {tree.sort()}")
+
+if __name__ == '__main__':
+	main()
